6609ebd710d902110b2f8c3a4f0ce78de07065ac
Gunjan Jamuar <gunjanjamuar@gmail.com>
2023-07-26 22:31:21 -0400

    merge whale data

--- merge_whale_data.py
+++ merge_whale_data.py
@@ -15,6 +15,28 @@ def merge_whale_data():
     print(df.columns.sort_values())
     print(df_2.columns.sort_values())
 
+    df_2.rename(columns={'Common Name': 'ComName', 'Latitude': 'lat', 'Longitude': 'lon', 'ObjectID': 'OBJECTID',
+                         'Obs Date': 'ObsDay', 'State/Province': 'State', }, inplace=True)
+
+    # After renaming the columns
+    print("\nAfter modifying column:\n", df_2.columns.sort_values())
+    df_2['Year'] = pd.to_datetime(df_2['ObsDay'], yearfirst=True, format='%B %d, %Y').dt.strftime('%Y%m%d')
+    print(df_2.head())
+
+    outer_merged = pd.merge(
+        df, df_2, how="inner", on=["Year", "State", ])  # 'lat', 'lon'
+    print(outer_merged.head(50))
+    print(outer_merged.shape)
+    print(outer_merged.columns)
+    new_df = outer_merged[['lat_x', 'lat_y']]
+    print(new_df.head(20))
+
+    new_df = outer_merged[['lat_x', 'lat_y', 'lon_x', 'lon_y', 'Year', 'County_x', 'County_y', 'State']]
+    print(new_df.head(20))
+
+    new_df = outer_merged[['lon_x', 'lon_y', 'County_x', 'County_y']]
+    print(new_df.head(20))
+
 
 if __name__ == '__main__':
     merge_whale_data()
