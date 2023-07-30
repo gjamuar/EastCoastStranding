6a06a14df5fde95dc0bf50343dabec895e51c544
Gunjan Jamuar <gunjanjamuar@gmail.com>
2023-07-30 11:55:28 -0400

    updated hover data for whales

--- vessels.py
+++ vessels.py
@@ -21,9 +21,9 @@ def map_vessels(vessels: pd.DataFrame, whales_df: pd.DataFrame):
                             # color_discrete_sequence=["fuchsia"],
                             zoom=4, height=1000, mapbox_style="open-street-map")
 
-    fig1 = px.scatter_mapbox(whales_df, lat="lat", lon="lon", hover_name="SpeciesCategory",
-                             hover_data=["NatlDBNum", "State", "SpeciesCategory", "County", 'Species', 'Genus', 'Date',
-                                         'State'],
+    fig1 = px.scatter_mapbox(whales_df, lat="lat", lon="lon", hover_name="ComName",
+                             hover_data=["State", "ComName", "County", 'Year_new',
+                                         "NatlDBNum", 'Field Number', 'Species', 'Genus'],
                              # animation_frame="Year",
                              color_discrete_sequence=["black"]
                              , zoom=4,
@@ -76,19 +76,20 @@ def parse_arg():
         This program displays Stranded whales and Vessels Traffic
         in north-east coast during selected time window.
         '''
-    parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter,
-                                        description= arg_desc)
-    
-    parser.add_argument("-ws", "--whale_sdate", help = "Start date for stranded whale time window in YYYY-MM-DD format")
-    parser.add_argument("-we", "--whale_edate", help = "End date for stranded whale time window in YYYY-MM-DD format")
-    parser.add_argument("-vs", "--vessel_sdate", help = "Start date for vessel traffic time window in YYYY-MM-DD format")
-    parser.add_argument("-ve", "--vessel_edate", help = "End date for vessel traffic time window in YYYY-MM-DD format")
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=arg_desc)
+
+    parser.add_argument("-ws", "--whale_sdate", help="Start date for stranded whale time window in YYYY-MM-DD format")
+    parser.add_argument("-we", "--whale_edate", help="End date for stranded whale time window in YYYY-MM-DD format")
+    parser.add_argument("-vs", "--vessel_sdate", help="Start date for vessel traffic time window in YYYY-MM-DD format")
+    parser.add_argument("-ve", "--vessel_edate", help="End date for vessel traffic time window in YYYY-MM-DD format")
     return vars(parser.parse_args())
+
 
 def read_whale_data(whale_start_date, whale_end_date):
     df_whales = load_from_file('merged_whales.parquet')
     df_whales['Year_new'] = pd.to_datetime(df_whales['Year'], yearfirst=True, format='%Y%m%d').dt.strftime(
-        '%Y-%m-%d')  
+        '%Y-%m-%d')
     # df_whales.sort_values(by=['Year_new'], ascending=True, na_position='first', inplace=True)
 
     df_whales_window: pd.DataFrame = df_whales.loc[df_whales["Year_new"].between(whale_start_date, whale_end_date)]
@@ -111,8 +112,8 @@ def read_traffic_data(vessel_start_date, vessel_end_date):
     print(f'df_vessel_window {df_vessel_window.shape}')
     return df_vessel_window
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     args = parse_arg()
     print(args)
 
@@ -127,52 +128,50 @@ if __name__ == '__main__':
 
     map_vessels(df_vessel_window, df_whales_window)
 
+# Create your connection.
 
- # Create your connection.
+# cnx = sqlite3.connect('aisdata_new.db')
+#
+# df = pd.read_sql_query("SELECT * FROM vessels", cnx)
+# print(df.head())
+# # for col in df.columns:
+# #     print(col)
+# df['geohash12'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=12), axis=1)
+# df['geohash11'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=11), axis=1)
+# df['geohash10'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=10), axis=1)
+# df['geohash9'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=9), axis=1)
+# df['geohash8'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=8), axis=1)
+# df['geohash7'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=7), axis=1)
+# df['geohash6'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=6), axis=1)
+# print(df[['lat', 'long', 'geohash12', 'geohash11', 'geohash10', 'geohash9', 'geohash8']].head())
+#
+# df.to_parquet('vessels.parquet')
 
-    # cnx = sqlite3.connect('aisdata_new.db')
-    #
-    # df = pd.read_sql_query("SELECT * FROM vessels", cnx)
-    # print(df.head())
-    # # for col in df.columns:
-    # #     print(col)
-    # df['geohash12'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=12), axis=1)
-    # df['geohash11'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=11), axis=1)
-    # df['geohash10'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=10), axis=1)
-    # df['geohash9'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=9), axis=1)
-    # df['geohash8'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=8), axis=1)
-    # df['geohash7'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=7), axis=1)
-    # df['geohash6'] = df.apply(lambda x: gh.encode(x.lat, x.long, precision=6), axis=1)
-    # print(df[['lat', 'long', 'geohash12', 'geohash11', 'geohash10', 'geohash9', 'geohash8']].head())
-    #
-    # df.to_parquet('vessels.parquet')
+# df = pd.read_parquet('vessels.parquet')
 
-    # df = pd.read_parquet('vessels.parquet')
+# df["Year"] = pd.to_datetime(df["datetime"]).dt.strftime('%Y-%m-%d')
 
-    # df["Year"] = pd.to_datetime(df["datetime"]).dt.strftime('%Y-%m-%d')
-    
-    
 
-    # # df2 = df.groupby(['date', 'geohash8'])['geohash8'].count().sort_values(ascending=False)
-    # # Group the DataFrame by team and position
-    # group = df.groupby(['date', 'geohash8'])
-    #
-    # # Count the number of players in each group
-    # count = group.size().sort_values(ascending=False)
-    #
-    # # Convert the count to a DataFrame
-    # df_count = count.reset_index(name='count')
-    #
-    # # View the DataFrame
-    # print(df_count)
-    # df_count['location'] = df_count.apply(lambda rec: gh.decode(rec['geohash8']), axis=1)
-    # # df_count['lat', 'long'] = gh.decode(df_count['geohash8'])
-    #
-    # print(df_count)
-    # print(gh.decode('dqgnj8es'))
-    #
-    # df3 = df.groupby(['date', 'geohash9'])['geohash9'].count().sort_values(ascending=False)
-    # print(df3)
-    #
-    # df4 = df.groupby(['date', 'geohash10'])['geohash10'].count().sort_values(ascending=False)
-    # print(df4)
+# # df2 = df.groupby(['date', 'geohash8'])['geohash8'].count().sort_values(ascending=False)
+# # Group the DataFrame by team and position
+# group = df.groupby(['date', 'geohash8'])
+#
+# # Count the number of players in each group
+# count = group.size().sort_values(ascending=False)
+#
+# # Convert the count to a DataFrame
+# df_count = count.reset_index(name='count')
+#
+# # View the DataFrame
+# print(df_count)
+# df_count['location'] = df_count.apply(lambda rec: gh.decode(rec['geohash8']), axis=1)
+# # df_count['lat', 'long'] = gh.decode(df_count['geohash8'])
+#
+# print(df_count)
+# print(gh.decode('dqgnj8es'))
+#
+# df3 = df.groupby(['date', 'geohash9'])['geohash9'].count().sort_values(ascending=False)
+# print(df3)
+#
+# df4 = df.groupby(['date', 'geohash10'])['geohash10'].count().sort_values(ascending=False)
+# print(df4)
