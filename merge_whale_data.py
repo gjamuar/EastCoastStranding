c52b0963634857ec195ee2fe38b01e8d0ee1c6e5
Gunjan Jamuar <gunjanjamuar@gmail.com>
2023-07-30 00:53:21 -0400

    merge whale data

--- merge_whale_data.py
+++ merge_whale_data.py
@@ -15,6 +15,24 @@ def merge_whale_data():
     print(df.columns.sort_values())
     print(df_2.columns.sort_values())
 
+    # Index(['ComName', 'ConfCode', 'County', 'Date', 'Genus', 'LatAcc', 'LongAcc',
+    #        'NatlDBNum', 'OBJECTID', 'ObsDay', 'ObsStatus', 'Species',
+    #        'SpeciesCategory', 'SpeciesID', 'State', 'WaterBody', 'Year', 'lat',
+    #        'lon', 'spatialReference', 'x', 'y'],
+    #       dtype='object')
+    # Index(['Carcass Condition', 'Common Name', 'Country', 'County', 'Field Number',
+    #        'Latitude', 'Longitude', 'Month', 'ObjectID', 'Obs Date', 'Sex',
+    #        'State/Province', 'Year'],
+    #       dtype='object')
+    #
+    # After
+    # modifying
+    # column:
+    # Index(['Carcass Condition', 'ComName', 'Country', 'County', 'Field Number',
+    #        'Month', 'OBJECTID', 'ObsDay', 'Sex', 'State', 'Year', 'lat', 'lon'],
+    #       dtype='object')
+    #
+
     df_2.rename(columns={'Common Name': 'ComName', 'Latitude': 'lat', 'Longitude': 'lon', 'ObjectID': 'OBJECTID',
                          'Obs Date': 'ObsDay', 'State/Province': 'State', }, inplace=True)
 
@@ -23,19 +41,38 @@ def merge_whale_data():
     df_2['Year'] = pd.to_datetime(df_2['ObsDay'], yearfirst=True, format='%B %d, %Y').dt.strftime('%Y%m%d')
     print(df_2.head())
 
+    df['County'] = df['County'].str.split("(").str[0].str.strip().str.lower()
+    df_2['County'] = df_2['County'].str.strip().str.lower()
+
+    print(df['County'].values)
+    print(df_2['County'].values)
+
+    # df = df.drop(columns=['Carcass Condition', 'Month', 'Sex'])
+
     outer_merged = pd.merge(
-        df, df_2, how="inner", on=["Year", "State", ])  # 'lat', 'lon'
+        df, df_2, how="outer", on=["Year", "State", "County"])  # 'lat', 'lon'
     print(outer_merged.head(50))
     print(outer_merged.shape)
     print(outer_merged.columns)
     new_df = outer_merged[['lat_x', 'lat_y']]
-    print(new_df.head(20))
+    print(new_df.head(50))
 
-    new_df = outer_merged[['lat_x', 'lat_y', 'lon_x', 'lon_y', 'Year', 'County_x', 'County_y', 'State']]
-    print(new_df.head(20))
+    new_df = outer_merged[['lat_x', 'lat_y', 'lon_x', 'lon_y', 'Year', 'County', 'State']]
+    print(new_df.head(50))
 
-    new_df = outer_merged[['lon_x', 'lon_y', 'County_x', 'County_y']]
-    print(new_df.head(20))
+    new_df = outer_merged[['lon_x', 'lon_y', 'County']]
+    print(new_df.head(50))
+
+    outer_merged['Lat_diff'] = outer_merged['lat_x'] - outer_merged['lat_y']
+    outer_merged['Lon_diff'] = outer_merged['lon_x'] - outer_merged['lon_y']
+
+    print(outer_merged['Lat_diff'].sort_values().values)
+    print(outer_merged['Lon_diff'].sort_values().values)
+
+    rslt_df = outer_merged[(abs(outer_merged['Lat_diff']) < 0.0001) & abs((outer_merged['Lon_diff'] < 0.0001))]
+
+    print('\nResult dataframe :\n', rslt_df[['lat_x', 'lat_y', 'lon_x', 'lon_y', 'Year', 'County', 'State','Year']].to_string())
+    print('\nResult dataframe :\n', rslt_df['Field Number'].values)
 
 
 if __name__ == '__main__':
